ee08153068e16b6d3b6cffd8dc7d8bde4d5a6d02
Dolan <dolan_miu@hotmail.com>
2017-10-23 23:58:38 +0100

    Add home component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { RoomModule } from './room/room.module';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { RoomModule } from './room/room.module';
         BrowserModule,
         AppRoutingModule,
         RoomModule,
+        HomeModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
