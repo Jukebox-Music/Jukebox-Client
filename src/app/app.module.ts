119bcb6f299f6b8bb1094799b30ecdb2fcd772d1
Dolan <dolan_miu@hotmail.com>
2017-10-23 21:01:50 +0100

    Add room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoomModule } from './room/room.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/room/room.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoomComponent } from './room.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    RoomComponent,
+  ]
+})
+export class RoomModule { }
