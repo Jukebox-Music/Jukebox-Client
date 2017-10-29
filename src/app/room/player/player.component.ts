eb85a98fe2b0ba485e48982126f09f2c590c2ffc
Dolan <dolan_miu@hotmail.com>
2017-10-29 02:01:49 +0000

    Fix linting

--- src/app/room/player/player.component.ts
+++ src/app/room/player/player.component.ts
@@ -3,7 +3,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-player',
     templateUrl: './player.component.html',
-    styleUrls: ['./player.component.scss']
+    styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements AfterViewInit {
 
