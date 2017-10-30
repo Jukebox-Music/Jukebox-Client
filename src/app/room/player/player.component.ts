41e791ba51c66902fc2d702799d9f8f4eea58eab
Dolan <dolan_miu@hotmail.com>
2017-10-30 01:56:52 +0000

    Add custom player

--- src/app/room/player/player.component.ts
+++ src/app/room/player/player.component.ts
@@ -8,52 +8,55 @@ import { environment } from '../../../environments/environment';
     styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements OnInit {
-    private serverUrl = environment.server.url;
-    @ViewChild('player') public player: ElementRef;
     @Output() public event = new EventEmitter<PlayerEvent>();
+    private audio: HTMLAudioElement;
 
     constructor() { }
 
     public ngOnInit(): void {
-        this.initPlayer();
+        // this.initPlayer();
+        this.audio = new Audio(`${environment.server.url}/song/test`);
+    }
+
+    public play(): void {
+        console.log('playing');
+        this.audio.play();
+        this.event.emit({
+            type: 'play',
+            seek: this.audio.currentTime,
+            state: 'play',
+        });
+    }
+
+    public pause(): void {
+        this.audio.pause();
+        this.event.emit({
+            type: 'pause',
+            seek: this.audio.currentTime,
+            state: 'pause',
+        });
+    }
+
+    public seek(): void {
+        this.event.emit({
+            type: 'seek',
+            seek: this.audio.currentTime,
+            state: 'pause',
+        });
     }
 
     public set PlayState(state: PlayState) {
         switch (state) {
             case 'play':
-                this.player.nativeElement.play();
+                this.audio.play();
                 break;
             case 'pause':
-                this.player.nativeElement.pause();
+                this.audio.pause();
                 break;
         }
     }
 
     public set Seek(seek: number) {
-        this.player.nativeElement.currentTime = seek;
-    }
-
-    private initPlayer(): void {
-        this.player.nativeElement.addEventListener('seeked', (e) => {
-            this.event.emit({
-                type: 'seek',
-                seek: e.srcElement.currentTime,
-                state: 'pause',
-            });
-        }, true);
-        this.player.nativeElement.addEventListener('play', (e) => {
-            this.event.emit({
-                type: 'play',
-                seek: e.srcElement.currentTime,
-                state: 'play',
-            });
-        }, true);
-        this.player.nativeElement.addEventListener('pause', (e) => {
-            this.event.emit({
-                type: 'pause',
-                seek: e.srcElement.currentTime,
-                state: 'pause',
-            });
-        }, true);
+        this.audio.currentTime = seek;
     }
 }
