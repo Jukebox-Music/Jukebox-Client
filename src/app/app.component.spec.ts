1e069173413b6493d78676b440813aa21edcc8a2
Dolan <dolan_miu@hotmail.com>
2017-11-05 02:02:39 +0000

    Add sticky footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,14 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent,
+                FooterComponent,
             ],
             imports: [
                 RouterTestingModule,
