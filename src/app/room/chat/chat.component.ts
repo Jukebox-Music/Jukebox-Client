548f80cb738327f2500dac788fe781f9c9c73dcc
Dolan <dolan_miu@hotmail.com>
2017-11-05 23:31:37 +0000

    Improve chat

--- src/app/room/chat/chat.component.ts
+++ src/app/room/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { SocketService } from '../../socket.service';
@@ -9,22 +9,27 @@ import { SocketService } from '../../socket.service';
     styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-    public chatLog: string[];
+    public chatLog: ChatMessage[];
     public text: string;
     public chat$: Observable<ChatMessage>;
+    @ViewChild('chat') public chat: ElementRef;
 
     constructor(private socketService: SocketService) {
         this.chatLog = [];
         this.chat$ = this.socketService.Socket$
             .ofType<ChatMessage>('chat-response')
-            .do((m) => console.log);
+            .do((m) => {
+                this.chatLog.push(m);
+                console.log(this.chatLog);
+            });
     }
 
     public ngOnInit(): void {
-
+        this.chat$.subscribe();
     }
 
     public submit(text: string): void {
         this.socketService.Socket.emit('chat-request', text);
+        this.text = '';
     }
 }
