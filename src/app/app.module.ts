d79d2b53c26535126a66428110ab63053f53a15e
Dolan <dolan_miu@hotmail.com>
2017-11-07 20:34:46 +0000

    Fix linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomeModule } from './home/home.module';
 import { RoomModule } from './room/room.module';
 import { SocketService } from './socket.service';
 import { AppStoreModule } from './store/app-store.module';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
