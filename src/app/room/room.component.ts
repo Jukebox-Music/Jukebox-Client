dc326ef3abf716dc7e56c42c770ac659db9c592f
Dolan <dolan_miu@hotmail.com>
2017-11-10 03:28:23 +0000

    Fix linting

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
