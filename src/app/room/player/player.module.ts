f66211f9401cdf2c668db993bb347c233d1118a2
Dolan <dolan_miu@hotmail.com>
2017-11-14 22:03:23 +0000

    Formatting

--- src/app/room/player/player.module.ts
+++ src/app/room/player/player.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
+
 import { PlayerComponent } from './player.component';
 import { SeekComponent } from './seek/seek.component';
 
